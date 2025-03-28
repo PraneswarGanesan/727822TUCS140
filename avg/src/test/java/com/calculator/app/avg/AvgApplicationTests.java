30c60afe6624e87248662fe31c814f4a4cb6078d
Praneswar G <praneswarganesan@gmail.com>
2025-03-28 13:45:32 +0530

    springcalcreated

---
+++ avg/src/test/java/com/calculator/app/avg/AvgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.app.avg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
